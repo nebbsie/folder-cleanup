31d9f8f6866452fc8b889fe44949eab5646e9979
nebbsie <aaron@nebbs.com>
2017-07-04 04:27:13 +0100

    removeed comments ready to add GUI and split code into objects

--- immaculate.py
+++ immaculate.py
@@ -20,9 +20,6 @@ else:
 
 
 def convert_bytes(num):
-    """
-    this function will convert bytes to MB.... GB... etc
-    """
     for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
@@ -30,9 +27,6 @@ def convert_bytes(num):
 
 
 def file_size(file_path):
-    """
-    this function will return the file size
-    """
     if os.path.isfile(file_path):
         file_info = os.stat(file_path)
         return file_info.st_size
